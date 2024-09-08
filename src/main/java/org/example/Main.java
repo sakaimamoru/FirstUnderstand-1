85e3f88cbbde511915bd8c0ab636336ba43ae20c
sakaimamoru <qrkm81222@yahoo.co.jp>
2024-09-08 09:58:02 +0900

    修正コミット-1 修正点 ・calculationクラスに0で割った時の挙動を追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,43 +1,47 @@
 package org.example;
 
+import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("1番目の数値を入力してください");
+    boolean running = true;
 
-    double num1 = scanner.nextDouble();
+    while (running) {
+      System.out.println("1番目の数値を入力してください");
 
-    System.out.println("1番目の数値が入力されました");
-    System.out.println("+, -, *, /いずれかの演算子を入力してください");
-    String operator = scanner.next();
+      double num1 = scanner.nextDouble();
 
-    System.out.println("2番目の数値を入力してください");
-    double num2 = scanner.nextDouble();
+      System.out.println("1番目の数値が入力されました");
+      System.out.println("+, -, *, /いずれかの演算子を入力してください");
+      String operator = scanner.next();
 
-    double result;
-    switch (operator) {
-      case "+":
-        result = calculation.add(num1, num2);
-        break;
-      case "-":
-        result = calculation.subtraction(num1, num2);
-        break;
-      case "*":
-        result = calculation.multiplication(num1, num2);
-        break;
-      case "/":
-        result = calculation.division(num1, num2);
-        break;
-      default:
-        System.out.println("対応していない演算子です");
-        return;
+      System.out.println("2番目の数値を入力してください");
+      double num2 = scanner.nextDouble();
+
+      double result;
+      switch (operator) {
+        case "+":
+          result = calculation.add(num1, num2);
+          break;
+        case "-":
+          result = calculation.subtraction(num1, num2);
+          break;
+        case "*":
+          result = calculation.multiplication(num1, num2);
+          break;
+        case "/":
+          result = calculation.division(num1, num2);
+        default:
+          System.out.println("対応していない演算子です");
+          return;
+      }
+
+      System.out.println("計算結果" + result);
+
+
     }
-
-    System.out.println("計算結果" + result);
-
-
   }
 }
--- src/main/java/org/example/calculation.java
+++ src/main/java/org/example/calculation.java
@@ -18,9 +18,15 @@ public class calculation {
   }
 
   public static double division(double num1, double num2) {
-    return num1 / num2;
+    if (num2 == 0) {
+      System.out.println("0で割る事はできません");
+      return 0;
+    } else {
+      return num1 / num2;
+
+    }
 
   }
-
-
 }
+
+
